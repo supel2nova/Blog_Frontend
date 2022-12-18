f37bc9128ea2976264e9ca58b4a3cf3c23ed343c
supel2nova <chal2aznable@gmail.com>
2022-12-19 00:33:32 +0700

    fix loginn

--- services/authorize.js
+++ services/authorize.js
@@ -2,7 +2,6 @@ export const authenticate = (response, next) => {
   if (window !== "undefined") {
     sessionStorage.setItem("token", JSON.stringify(response.data.token));
     sessionStorage.setItem("user", JSON.stringify(response.data.username));
-    window.location.reload();
   }
   next();
 };
