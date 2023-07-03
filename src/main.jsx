7f7b9ca3a98b6505b859ba7508953c789a8c301c
Panupong Yapradith <Yew@odds.team>
2023-07-03 21:49:44 +0700

    add: chat facebook

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Myroutes from "./Myroutes";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <MessengerCustomerChat
+      pageId="101439554570293"
+      appId="195438366825122"
+    />
+    ,
     <Myroutes />
   </React.StrictMode>
 );
