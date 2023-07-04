0555aaf212c50cebfb50d948d22a4730ffef5558
Panupong Yapradith <Yew@odds.team>
2023-07-04 17:37:06 +0700

    sss

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { MessengerChat } from "react-messenger-chat-plugin";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MessengerChat
-      pageId="101439554570293"
+      pageId="1652647158184149"
       language="th_TH"
       loggedInGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
       loggedOutGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
