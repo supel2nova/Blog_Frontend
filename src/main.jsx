c213c09830ed71b3d5a808689f4f10a975239b54
Panupong Yapradith <Yew@odds.team>
2023-07-04 16:28:37 +0700

    sss

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Myroutes from "./Myroutes";
 import MessengerCustomerChat from "react-messenger-customer-chat";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MessengerCustomerChat
+    <MessengerChat
       pageId="101439554570293"
-      appId="195438366825122"
+      language="th_TH"
+      themeColor={"#000000"}
+      bottomSpacing={300}
       loggedInGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
+      loggedOutGreeting="loggedOutGreeting"
+      greetingDialogDisplay={"show"}
+      debugMode={true}
+      onMessengerShow={() => {
+        console.log("onMessengerShow");
+      }}
+      onMessengerHide={() => {
+        console.log("onMessengerHide");
+      }}
+      onMessengerDialogShow={() => {
+        console.log("onMessengerDialogShow");
+      }}
+      onMessengerDialogHide={() => {
+        console.log("onMessengerDialogHide");
+      }}
+      onMessengerMounted={() => {
+        console.log("onMessengerMounted");
+      }}
+      onMessengerLoad={() => {
+        console.log("onMessengerLoad");
+      }}
     />
-    ,
     <Myroutes />
   </React.StrictMode>
 );
