178f7188fc3c597cf2f518d1f94746d2b6edaff3
Panupong Yapradith <Yew@odds.team>
2023-07-05 13:37:18 +0700

    test env

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Myroutes from "./Myroutes";
 import { MessengerChat } from "react-messenger-chat-plugin";
+const pageId = process.env.FACEBOOK_CHAT;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MessengerChat
-       pageId={import.meta.env.FACEBOOK_CHAT}
+      pageId={pageId}
       language="th_TH"
       themeColor={"#ffb21d"}
       loggedInGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
