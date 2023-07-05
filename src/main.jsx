186d07c9f18977b72b5f95b5d56e2e192d0e0720
Panupong Yapradith <chal2aznable@gmail.com>
2023-07-05 12:27:15 +0700

    change color

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Myroutes from "./Myroutes";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 import { MessengerChat } from "react-messenger-chat-plugin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,6 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <MessengerChat
       pageId="1652647158184149"
       language="th_TH"
+      themeColor={"#ffb21d"}
       loggedInGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
       loggedOutGreeting="ยินดีต้อนรับสู่เว็บ insKru ค่ะ พบปัญหาเว็บไซต์ติดต่อได้ทางช่องทางนี้เลยค่า :)"
       greetingDialogDisplay={"show"}
