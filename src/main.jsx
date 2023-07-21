e3c39c5e80e6a20df0e04f47d22b656bce552025
Panupong Yapradith <Yew@odds.team>
2023-07-21 17:21:04 +0700

    change path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Myroutes from "./Myroutes";
-import FacebookChat from "./FacebookChat";
+import FacebookChat from "./components/FacebookChat";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
